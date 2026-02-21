361de6feadd8a24be93089cb230534dea7e0e4ba
BallaHalls <leonardo.v070808@gmail.com>
2026-02-21 19:11:07 -0300

    Commit diario

--- Coisas/CPFgen.c
+++ Coisas/CPFgen.c
@@ -1,81 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <windows.h>
 #include <time.h>
+#include <stdbool.h>
 
-int main()
-{
-    label:
+int main(){
+
     srand(time(NULL));
-    static int tries, location, ran, ver1, ver2;
+    int iteration = 0, location = 0;
+    bool debug = 1;
     int nums[11] = {0};
     int size = sizeof(nums) / sizeof(nums[0]);
+
     do{
-    system("cls");
-    printf("0: RS\n1: DF, GO, MS, MT, TO\n2: AC, AM, AP, PA, RO, RR\n3: CE, MA, PI\n4: AL, PB, PE, RN\n5: BA, SE\n6: MG\n7: ES, RJ\n8: SP\n9: PR, SC\nLocation: ");
-    scanf("%d", &location);
-    }while(location < 0 || location > 9);
+        system("cls");
+        printf("0: RS\n1: DF, GO, MS, MT, TO\n2: AC, AM, AP, PA, RO, RR\n3: CE, MA, PI\n4: AL, PB, PE, RN\n5: BA, SE\n6: MG\n7: ES, RJ\n8: SP\n9: PR, SC\nLocation: ");
+        scanf("%d", &location);
+    } while (location < 0 || location > 9);
 
     system("cls");
 
-    do
-    {   
+        do{
         int soma = 0;
-        for (int i = 0; i < 9; i++)
-        { // gera os 9 digitos
-            nums[i] = rand() % 10;
-            // printf("%d", nums[i]);
-            soma += nums[i] * (10 - i); // calcula a soma
-        }
 
-        if (soma % 11 == 0 || soma % 11 == 1)
-        {
-            // ver1 = 0;
+        for (int i = 0; i < 9; i++){
+            nums[i] = rand() % 10;
+            soma += nums[i] * (10 - i);
+        }        
+
+        if (soma % 11 == 0 || soma % 11 == 1){
             nums[9] = 0;
-        }
-        else
-        {
-            // ver1 = (11 - (soma % 11));
+        }else{
             nums[9] = (11 - (soma % 11));
         }
 
-        // printf("-%d", ver1);
         soma = 0;
 
-        for (int i = 0; i < 10; i++)
-        {
+        for (int i = 0; i < 10; i++){
             soma += nums[i] * (11 - i);
         }
 
-        if (soma % 11 == 0 || soma % 11 == 1)
-        {
-            // ver2 = 0;
+        if (soma % 11 == 0 || soma % 11 == 1){
             nums[10] = 0;
-        }
-        else
-        {
-            // ver2 = (11 - (soma % 11));
+        }else{
             nums[10] = (11 - (soma % 11));
         }
-        tries++;
 
+        iteration++;
+        }while (nums[8] != location);
 
-    } while (nums[8] != location);
+    system("cls");
 
-        system("cls");
-        // printf("%d\n", ver2);
-
-        for (int i = 0; i < size; i++)
-    {   
-        if(i == 3 || i == 6){
+    for (int i = 0; i < size; i++){
+        if (i == 3 || i == 6)
+        {
             printf(".");
-        }if(i == 9){
-            printf("-");
+            Sleep(50);
         }
+        if (i == 9)
+        {
+            printf("-");
+            Sleep(50);
+        }   
         printf("%d", nums[i]);
         Sleep(50);
     }
-   (tries >= 2) ? printf("\n%d Iterations.", tries) : printf("%d Iteration", tries);
-    
+
+    (iteration >= 2) ? printf("\n%d Iterations.", iteration) : printf("\n%d Iteration", iteration);
 }
---
+++ Testes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int adress = 1;
+
+    printf("%p", &adress);
+
+
+
+}
