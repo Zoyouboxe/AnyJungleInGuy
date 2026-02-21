e33828f9f50cab187f75ab9ba5ab5119d11e3711
BallaHalls <leonardo.v070808@gmail.com>
2026-02-21 20:25:06 -0300

    a

--- Testes.c
+++ Testes.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 int main(){
 
-    int adress = 1;
+    int vars[3] = {0};
+    char alpha[3] = {'a', 'b', 'c'};
+    float delta = 0.0f;
+    
+    for(int i = 0; i < 3; i++){
+        printf("%c = ", alpha[i]);
+        scanf("%d", &vars[i]);
+    }
 
-    printf("%p", &adress);
+    delta = vars[1] * vars[1] - 4.0 * vars[0] * vars[2];
+    
+    printf("%.2f", delta);
+
+    
 
 
 
-}
+
+
+
+
+
+}   
